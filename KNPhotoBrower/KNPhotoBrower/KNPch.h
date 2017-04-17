f9f2f5d7bfbdcda22a077a13858734d22ee221fd
LuKane <1169604556@qq.com>
2017-04-17 17:15:52 +0800

    将 图片的最大放大倍数和最小放大倍数 写在固定的地方
    将 图片的最大放大倍数和最小放大倍数 写在固定的地方

--- KNPhotoBrower/KNPhotoBrower/KNPch.h
+++ KNPhotoBrower/KNPhotoBrower/KNPch.h
@@ -18,6 +18,11 @@
 #define PhotoBrowerBrowerTime      .3f
 #define PhotoBrowerMargin          20.f
 
+// 图片的最大放大倍数
+#define PhotoBrowerImageMaxScale   2.f
+// 图片的最小缩小倍数
+#define PhotoBrowerImageMinScale   1.f
+
 #define PhotoSaveImageSuccessMessage  @"^_^ 保存成功!!"
 #define PhotoSaveImageFailureMessage @"/(ㄒoㄒ)/~~ 保存失败!!"
 #define PhotoSaveImageMessageTime    2
