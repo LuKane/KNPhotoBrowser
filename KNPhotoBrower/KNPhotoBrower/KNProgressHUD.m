ffc4b4db7720a96ff61056696b469b4e41a1ebc7
LuKane <1169604556@qq.com>
2017-08-17 15:38:52 +0800

    修改 加载圈的形式:增加类似微信方式加载
    修改 加载圈的形式:增加类似微信方式加载

--- KNPhotoBrower/KNPhotoBrower/KNProgressHUD.m
+++ KNPhotoBrower/KNPhotoBrower/KNProgressHUD.m
@@ -8,121 +8,114 @@
 
 #import "KNProgressHUD.h"
 
-
 @interface KNProgressHUD(){
     CGFloat       _width;
     CGFloat       _height;
+    CGFloat       _X;
+    CGFloat       _Y;
     CGFloat       _bordLineWidth;
-    CAShapeLayer *_sectorLayer;
-    CAShapeLayer *_borderLayer;
-    UIBezierPath *_sectorBezierPath;
 }
+
+/* 周边圆圈 */
+@property (nonatomic, strong) CAShapeLayer *sectorLayer;
+/* 加载圈 */
+@property (nonatomic, strong) CAShapeLayer *loadingLayer;
+/* 扇形圈 */
+@property (nonatomic, strong) CAShapeLayer *sharpLayer;
 
 @end
 
 @implementation KNProgressHUD
 
-static CGSize _superViewSize;
-static bool   _animated;
-
-+ (instancetype)showHUDAddTo:(UIView *)superView animated:(BOOL)animated{
-    _animated = animated;
-    KNProgressHUD *hud = [[self alloc] initWithSupView:superView];
-    
-    [superView addSubview:hud];
-    
-    return hud;
-}
-
-- (instancetype)initWithSupView:(UIView *)superView{
-    NSAssert(superView, @"superView can not be nil");
-    BOOL isOk = [self judgeSuperViewHasFrame:superView];
-    NSAssert(isOk, @"superView needs frame");
-    _superViewSize = (CGSize){superView.frame.size.width,superView.frame.size.height};
-    return [self init];
-}
-- (instancetype)init{
-    if (self = [super init]) {
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
         [self setBackgroundColor:[UIColor clearColor]];
-        [self initDefaultData];
     }
     return self;
 }
-#pragma mark - 初始化默认数据
-- (void)initDefaultData{
-    _width = 22;
-    _height = _width;
-    _bordLineWidth = 1.5f;
-    _HUDColor = [UIColor whiteColor];
-    _sectorBoldColor = _HUDColor;
-    _sectorColor = _HUDColor;
-    _progress = 0.f;
-    
-    [self loadSubViews];
-}
-#pragma mark - 加载所有子图层
-- (void)loadSubViews{
-    
-    [self setFrame:CGRectMake(0, 0, (_width + _bordLineWidth * 2 ) * 2,(_height + _bordLineWidth * 2) * 2)];
-    [self setCenter:CGPointMake(_superViewSize.width * 0.5, _superViewSize.height * 0.5)];
-    
-    // 1.扇形
-    CAShapeLayer *sectorLayer = [CAShapeLayer layer];
-    [sectorLayer setFillColor:nil];
-    [sectorLayer setStrokeColor:[_sectorBoldColor CGColor]];
-    sectorLayer.lineWidth = _width;
-    _sectorLayer = sectorLayer;
-    [self.layer addSublayer:sectorLayer];
-    
-    // 2.边框
-    CAShapeLayer *borderLayer = [CAShapeLayer layer];
-    [borderLayer setFillColor:nil];
-    [borderLayer setStrokeColor:[_sectorBoldColor CGColor]];
-    [borderLayer setPath:[[UIBezierPath bezierPathWithArcCenter:(CGPoint){_width + _bordLineWidth * 2, _height + _bordLineWidth * 2} radius:(_width + _bordLineWidth * 2.f) startAngle:0 endAngle:M_PI * 2 clockwise:YES] CGPath]];
-    [borderLayer setLineWidth:_bordLineWidth];
-    _borderLayer = borderLayer;
-    [self.layer addSublayer:borderLayer];
-}
-#pragma mark - 判断父控件是否为空或不合理
-- (BOOL)judgeSuperViewHasFrame:(UIView *)superView{
-    if(superView.frame.size.width <= 0 || superView.frame.size.height <= 0){
-        return NO;
-    }
-    return YES;
+
+- (void)drawRect:(CGRect)rect{
+    [self setupSectorLayer];
+    [self setupLoadingLayer];
+    [self setupSharpLayer:rect];
 }
 
-#pragma mark - 设定 进度
+// 周边圆圈
+- (void)setupSectorLayer{
+    self.sectorLayer= [CAShapeLayer layer];
+    [self.sectorLayer setFillColor:[UIColor clearColor].CGColor];
+    [self.sectorLayer setLineWidth:1.f];
+    [self.sectorLayer setStrokeColor:[UIColor groupTableViewBackgroundColor].CGColor];
+    [self.sectorLayer setPath:[[UIBezierPath bezierPathWithOvalInRect:[self bounds]] CGPath]];
+    [self.sectorLayer setHidden:YES];
+    [self.layer addSublayer:self.sectorLayer];
+}
+// 加载圈
+- (void)setupLoadingLayer{
+    self.loadingLayer = [CAShapeLayer layer];
+    [self.loadingLayer setFrame:[self bounds]];
+    [self.loadingLayer setAnchorPoint:CGPointMake(0.5f, 0.5f)];
+    [self.loadingLayer setFillColor:[[UIColor clearColor] CGColor]];
+    [self.loadingLayer setLineWidth:1.f];
+    [self.loadingLayer setStrokeColor:[[UIColor groupTableViewBackgroundColor] CGColor]];
+    
+    CGPoint center = CGPointMake(self.bounds.size.width * 0.5, self.bounds.size.height * 0.5);
+    CGFloat loadRadius = self.bounds.size.width * 0.5;
+    CGFloat endAngle = (2 * (float)M_PI) - ((float)M_PI / 8);
+    
+    self.loadingLayer.path = [UIBezierPath bezierPathWithArcCenter:center
+                                                            radius:loadRadius
+                                                        startAngle:0
+                                                          endAngle:endAngle
+                                                         clockwise:YES].CGPath;
+    
+    CABasicAnimation   *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    [rotationAnimation setToValue:[NSNumber numberWithFloat:M_PI * 2.0]];
+    [rotationAnimation setDuration:1.f];
+    [rotationAnimation setCumulative:YES];
+    [rotationAnimation setRepeatCount:HUGE_VALF];
+    [self.loadingLayer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+    [self.layer addSublayer:self.loadingLayer];
+}
+// 扇形圈
+- (void)setupSharpLayer:(CGRect)rect{
+    CGFloat minSide = MIN(CGRectGetWidth(rect), CGRectGetHeight(rect));
+    CGFloat radius  = minSide/2 - 3;
+    
+    self.sharpLayer = [CAShapeLayer layer];
+    [self.sharpLayer setFrame:[self bounds]];
+    [self.sharpLayer setAnchorPoint:CGPointMake(0.5f, 0.5f)];
+    [self.sharpLayer setFillColor:[[UIColor clearColor] CGColor]];
+    [self.sharpLayer setStrokeColor:[[UIColor groupTableViewBackgroundColor] CGColor]];
+    [self.sharpLayer setLineWidth:radius];
+
+    [self.sharpLayer setStrokeStart:0];
+    [self.sharpLayer setStrokeEnd:0];
+
+    CGRect pathRect = CGRectMake(CGRectGetWidth(self.bounds)/2 - radius/2, CGRectGetHeight(self.bounds)/2 - radius/2, radius, radius);
+    [self.sharpLayer setPath:[[UIBezierPath bezierPathWithRoundedRect:pathRect cornerRadius:radius] CGPath]];
+    [self.layer addSublayer:self.sharpLayer];
+}
+// 设置进度
 - (void)setProgress:(CGFloat)progress{
-    if(progress > 1) return;
-    _sectorBezierPath = [UIBezierPath bezierPathWithArcCenter:(CGPoint){_width + _bordLineWidth * 2, _height + _bordLineWidth * 2} radius:_width * 0.5 startAngle:-M_PI_2 endAngle:-M_PI_2 + progress * M_PI * 2 clockwise:YES];
-    [_sectorLayer setPath:[_sectorBezierPath CGPath]];
+    progress = MAX(0.0f, progress);
+    progress = MIN(1.0f, progress);
     
-    if(progress == 1){
-        if(!_animated) {
+    if (progress > 0) {
+        [self.loadingLayer removeAllAnimations];
+        self.sectorLayer.hidden = NO;
+        [self.loadingLayer removeFromSuperlayer];
+    }
+    
+    if (progress != _progress) {
+        self.sharpLayer.strokeEnd = progress;
+        _progress = progress;
+    }
+    
+    if (progress >= 1) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self removeFromSuperview];
-            return;
-        }
-        [UIView animateWithDuration:0.3f animations:^{
-            [self setAlpha:0.f];
-        } completion:^(BOOL finished) {
-            [self removeFromSuperview];
-        }];
+        });
     }
 }
-#pragma mark - 设定 颜色
-- (void)setHUDColor:(UIColor *)HUDColor{
-    [_sectorLayer setStrokeColor:[HUDColor CGColor]];
-    [_borderLayer setStrokeColor:[HUDColor CGColor]];
-}
-- (void)setSectorBoldColor:(UIColor *)sectorBoldColor{
-    [_borderLayer setStrokeColor:[sectorBoldColor CGColor]];
-}
-- (void)setSectorColor:(UIColor *)sectorColor{
-    [_sectorLayer setStrokeColor:[sectorColor CGColor]];
-}
-
-- (void)dismiss{
-    [self removeFromSuperview];
-}
-
 @end
