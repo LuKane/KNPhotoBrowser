e2eecc37fd80a05abd687913a3db91f6286291b8
LuKane <1169604556@qq.com>
2018-06-12 17:10:19 +0800

    新增API, 处理屏幕旋转
    新增API, 处理屏幕旋转

--- KNPhotoBrower/KNPhotoBrower/KNPhotoBrower.m
+++ KNPhotoBrower/KNPhotoBrower/KNPhotoBrower.m
@@ -240,15 +240,7 @@ static NSString *ID = @"KNCollectionView";
 #pragma mark - 初始化 pageView
 - (void)initializePageView{
     KNPhotoBrowerNumView *numView = [[KNPhotoBrowerNumView alloc] init];
-    CGFloat y = 25;
-<<<<<<< HEAD
-    if (iPhoneX) {
-=======
-    if(iPhoneX){
->>>>>>> origin/master
-        y = 45;
-    }
-    [numView setFrame:(CGRect){{0,y},{ScreenWidth,25}}];
+    [numView setFrame:(CGRect){{0,iPhoneX?45:25},{ScreenWidth,25}}];
     [numView setCurrentNum:(_currentIndex + 1) totalNum:_itemsArr.count];
     _page = [numView currentNum];
     [numView setHidden:!_isNeedPageNumView];
@@ -287,17 +279,7 @@ static NSString *ID = @"KNCollectionView";
     [operationBtn setBackgroundColor:[UIColor blackColor]];
     [operationBtn setAlpha:0.4];
     [operationBtn setBackgroundImage:[UIImage imageNamed:@"KNPhotoBrower.bundle/more_tap@2x.png"] forState:UIControlStateNormal];
-<<<<<<< HEAD
     [operationBtn setFrame:(CGRect){{ScreenWidth - 35 - 15,iPhoneX?45:25},{35,20}}];
-=======
-    
-    CGFloat y = 25;
-    if(iPhoneX){
-        y = 45;
-    }
-    
-    [operationBtn setFrame:(CGRect){{ScreenWidth - 35 - 15,y},{35,20}}];
->>>>>>> origin/master
     [operationBtn addTarget:self action:@selector(operationBtnIBAction) forControlEvents:UIControlEventTouchUpInside];
     [operationBtn setHidden:!_isNeedRightTopBtn];
     _operationBtn = operationBtn;
