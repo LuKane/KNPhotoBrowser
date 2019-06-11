8b978709bff5d1326e264a416aae6a22d2250a54
LuKane <1169604556@qq.com>
2019-06-11 19:37:09 +0800

    video player
    video player

---
+++ KNPhotoBrowser/KNPhotoBrowser/KNPhotoVideoCell.h
@@ -0,0 +1,17 @@
+//
+//  KNPhotoVideoCell.h
+//  KNPhotoBrowser
+//
+//  Created by LuKane on 2019/6/11.
+//  Copyright © 2019 LuKane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KNPhotoVideoCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KNPhotoBrowser/KNPhotoBrowser/KNPhotoVideoCell.m
@@ -0,0 +1,13 @@
+//
+//  KNPhotoVideoCell.m
+//  KNPhotoBrowser
+//
+//  Created by LuKane on 2019/6/11.
+//  Copyright © 2019 LuKane. All rights reserved.
+//
+
+#import "KNPhotoVideoCell.h"
+
+@implementation KNPhotoVideoCell
+
+@end
