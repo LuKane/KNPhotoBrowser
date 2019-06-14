35dc08f3286f5584e5ad5227f8db56388e4bbcbe
LuKane <1169604556@qq.com>
2019-06-14 17:09:07 +0800

    PlayerView
    PlayerView

---
+++ KNPhotoBrowser/KNPhotoBrowser/KNPhotoAVPlayerView.h
@@ -0,0 +1,19 @@
+//
+//  KNPhotoAVPlayerView.h
+//  KNPhotoBrowser
+//
+//  Created by LuKane on 2019/6/14.
+//  Copyright © 2019 LuKane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KNPhotoAVPlayerView : UIView
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KNPhotoBrowser/KNPhotoBrowser/KNPhotoAVPlayerView.m
@@ -0,0 +1,15 @@
+//
+//  KNPhotoAVPlayerView.m
+//  KNPhotoBrowser
+//
+//  Created by LuKane on 2019/6/14.
+//  Copyright © 2019 LuKane. All rights reserved.
+//
+
+#import "KNPhotoAVPlayerView.h"
+
+@implementation KNPhotoAVPlayerView
+
+
+
+@end
