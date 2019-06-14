18fcf30c3b07b5e3ebb9daa663b72538a0f2879b
LuKane <1169604556@qq.com>
2019-06-14 17:08:57 +0800

    actionView
    actionView

---
+++ KNPhotoBrowser/KNPhotoBrowser/KNPhotoAVPlayerActionView.m
@@ -0,0 +1,15 @@
+//
+//  KNPhotoAVPlayerActionView.m
+//  KNPhotoBrowser
+//
+//  Created by LuKane on 2019/6/14.
+//  Copyright © 2019 LuKane. All rights reserved.
+//
+
+#import "KNPhotoAVPlayerActionView.h"
+
+@implementation KNPhotoAVPlayerActionView
+
+
+
+@end
