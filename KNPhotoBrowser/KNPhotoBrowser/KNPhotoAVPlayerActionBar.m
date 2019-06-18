1486bd6f3372bc37976c2b00c602dc03ba7d2bdc
LuKane <1169604556@qq.com>
2019-06-18 18:35:39 +0800

    slider finished
    slider finished

--- KNPhotoBrowser/KNPhotoBrowser/KNPhotoAVPlayerActionBar.m
+++ KNPhotoBrowser/KNPhotoBrowser/KNPhotoAVPlayerActionBar.m
@@ -14,7 +14,24 @@
 
 @implementation KNPhotoAVPlayerSlider
 
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self setThumbImage:[UIImage imageNamed:@"KNPhotoBrowser.bundle/circlePoint@2x.png"] forState:UIControlStateNormal];
+        [self setMinimumTrackTintColor:[UIColor whiteColor]];
+        [self setMaximumTrackTintColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5]];
+    }
+    return self;
+}
 
+- (CGRect)trackRectForBounds:(CGRect)bounds{
+    CGRect frame = [super trackRectForBounds:bounds];
+    return CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 2);
+}
+
+- (CGRect)thumbRectForBounds:(CGRect)bounds trackRect:(CGRect)rect value:(float)value {
+    CGRect frame = [super thumbRectForBounds:bounds trackRect:rect value:value];
+    return CGRectMake(frame.origin.x - 10, frame.origin.y - 10, frame.size.width + 20, frame.size.height + 20);
+}
 
 @end
 
