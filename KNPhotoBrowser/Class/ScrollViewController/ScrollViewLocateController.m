cc704e28d57602fa61e6ebd0f6c8a29327be3382
LuKane <1169604556@qq.com>
2021-05-20 19:55:26 +0800

    scrollView locate

--- KNPhotoBrowser/Class/ScrollViewController/ScrollViewLocateController.m
+++ KNPhotoBrowser/Class/ScrollViewController/ScrollViewLocateController.m
@@ -7,26 +7,102 @@
 //
 
 #import "ScrollViewLocateController.h"
+#import "KNPhotoBrowser.h"
 
-@interface ScrollViewLocateController ()
+@interface ScrollViewLocateController ()<UIScrollViewDelegate,KNPhotoBrowserDelegate>
+
+@property (nonatomic,weak  ) UIScrollView *scrollView;
+@property (nonatomic, strong) NSMutableArray *dataArr;
+@property (nonatomic, strong) NSMutableArray *itemsArr;
 
 @end
 
 @implementation ScrollViewLocateController
 
+- (instancetype)init{
+    if (self = [super init]) {
+        NSArray *urlArr = @[
+                            [UIImage imageNamed:@"1.jpg"],
+                            [UIImage imageNamed:@"2.jpg"],
+                            [UIImage imageNamed:@"3.jpg"],
+                            [UIImage imageNamed:@"4.jpg"],
+                            [UIImage imageNamed:@"5.jpg"],
+                            [UIImage imageNamed:@"6.jpg"],
+                            [UIImage imageNamed:@"7.jpg"],
+                            [UIImage imageNamed:@"8.jpg"],
+                            [UIImage imageNamed:@"9.jpg"],
+                            [UIImage imageNamed:@"10.jpg"],
+                            [UIImage imageNamed:@"11.jpg"],
+                            [UIImage imageNamed:@"12.jpg"],
+                            [UIImage imageNamed:@"13.jpg"],
+                            [UIImage imageNamed:@"14.jpg"],
+                            ];
+        self.dataArr = [urlArr mutableCopy];
+    }
+    return self;
+}
+
+- (NSMutableArray *)itemsArr{
+    if (!_itemsArr) {
+        _itemsArr = [NSMutableArray array];
+    }
+    return _itemsArr;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = @"loc: Photo";
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    [self setupScrollView];
+    
+    if (@available(iOS 11.0, *)){
+        _scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setupScrollView{
+    
+    CGFloat y = 10;
+    CGFloat width = 180;
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, width + y * 2)];
+    [scrollView setDelegate:self];
+    [scrollView setShowsHorizontalScrollIndicator:NO];
+    [scrollView setShowsVerticalScrollIndicator:NO];
+    [scrollView setContentSize:(CGSize){(width + y * 2) * self.dataArr.count,0}];
+    [scrollView setBackgroundColor:[UIColor orangeColor]];
+    [self.view addSubview:scrollView];
+    _scrollView = scrollView;
+    
+    for (NSInteger i = 0; i < self.dataArr.count; i++) {
+        UIImageView *imageView = [[UIImageView alloc] init];
+        [imageView setUserInteractionEnabled:YES];
+        [imageView setBackgroundColor:[UIColor grayColor]];
+        [imageView setTag:i];
+        [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(imageViewIBAction:)]];
+        CGFloat x = (y * 2 + width) * i + 10;
+        imageView.image = self.dataArr[i];
+        imageView.frame = CGRectMake(x, y, width, width);
+        [scrollView addSubview:imageView];
+        
+        KNPhotoItems *items = [[KNPhotoItems alloc] init];
+        items.sourceView = imageView;
+        [self.itemsArr addObject:items];
+    }
 }
-*/
+
+- (void)imageViewIBAction:(UITapGestureRecognizer *)tap{
+    KNPhotoBrowser *photoBrowser = [[KNPhotoBrowser alloc] init];
+    photoBrowser.itemsArr = [_itemsArr copy];
+    photoBrowser.currentIndex = tap.view.tag;
+
+    photoBrowser.isNeedPageNumView = true;
+    photoBrowser.isNeedRightTopBtn = true;
+    photoBrowser.isNeedLongPress = true;
+    photoBrowser.isNeedPanGesture = true;
+    photoBrowser.isNeedPageControl = true;
+    
+    [photoBrowser presentOn:self];
+}
 
 @end
